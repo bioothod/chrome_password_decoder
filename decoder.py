81f2f791ab676e5e203b0b027238a91dd0c1166f
Evgeniy Polyakov <zbr@ioremap.net>
2023-02-03 13:42:48 +0000

    Added libsecret support for v11 encoded passwords

--- decoder.py
+++ decoder.py
@@ -1,12 +1,18 @@
 from typing import List, Dict, Union
 
 import argparse
+import base64
+import binascii
 import re
 import sqlite3
 
 from collections import namedtuple
-from hashlib import pbkdf2_hmac
 from Crypto.Cipher import AES
+from hashlib import pbkdf2_hmac
+
+import gi
+gi.require_version('Secret', '1')
+from gi.repository import Secret
 
 def unpad_pkcs5(data: bytes, block_size: int) -> bytes:
     pdata_len = len(data)
@@ -14,6 +20,17 @@ def unpad_pkcs5(data: bytes, block_size: int) -> bytes:
     if pdata_len < padding_len or padding_len > block_size:
         return data
     return data[:-padding_len]
+
+def decode_and_unpack(decoder_key: bytes, password: bytes) -> str:
+    iv = bytes(' ' * 16, 'ascii')
+    cipher = AES.new(decoder_key, AES.MODE_CBC, iv)
+
+    decoded_password = cipher.decrypt(password)
+    decoded_password = unpad_pkcs5(decoded_password, AES.block_size)
+    try:
+        return decoded_password.decode('ascii')
+    except UnicodeDecodeError:
+        return ''
 
 class DecoderV10:
     def __init__(self):
@@ -23,29 +40,49 @@ class DecoderV10:
 
         self.decoder_key = pbkdf2_hmac('sha1', password, salt, num_iterations, 16)
 
-    def decode(self, password: bytes) -> bytes:
-        iv = bytes(' ' * 16, 'ascii')
-        cipher = AES.new(self.decoder_key, AES.MODE_CBC, iv)
-
-        decoded_password = cipher.decrypt(password)
-        decoded_password = unpad_pkcs5(decoded_password, AES.block_size)
-        return decoded_password
+    def decode(self, password: bytes) -> List[str]:
+        return [decode_and_unpack(self.decoder_key, password)]
 
 class DecoderV11:
     def __init__(self):
         salt = b'saltysalt'
-        password = b'peanuts'
         num_iterations = 1
 
-        self.decoder_key = pbkdf2_hmac('sha1', password, salt, num_iterations, 16)
+        item_names = [
+            'Chrome Safe Storage',
+            'Chromium Safe Storage',
+        ]
 
-    def decode(self, password: bytes) -> bytes:
-        iv = bytes(' ' * 16, 'ascii')
-        cipher = AES.new(self.decoder_key, AES.MODE_CBC, iv)
+        self.secrets = []
 
-        decoded_password = cipher.decrypt(password)
-        decoded_password = unpad_pkcs5(decoded_password, AES.block_size)
-        return decoded_password
+        service = Secret.Service.get_sync(Secret.ServiceFlags.LOAD_COLLECTIONS)
+        service.load_collections()
+        for collection in service.get_collections():
+            collection_label = collection.get_label()
+            collection.load_items()
+            for item in collection.get_items():
+                item_label = item.get_label()
+                if item_label not in item_names:
+                    print(f'collection: {collection_label}, item: {item_label}')
+                    continue
+
+                item.load_secret_sync()
+                item_value = item.get_secret().get_text()
+                master_key = base64.b64decode(item_value)
+
+                decoder_key = pbkdf2_hmac('sha1', master_key, salt, num_iterations, 16)
+                print(f'collection: {collection_label}, item: {item_label}, secret: {item_value}, master_key: {binascii.hexlify(master_key)}, decoder_key: {binascii.hexlify(decoder_key)}')
+                self.secrets.append(decoder_key)
+
+    def decode(self, password: bytes) -> List[str]:
+        decoded_passwords: List[str] = []
+
+        for decoder_key in self.secrets:
+            decoded_password = decode_and_unpack(decoder_key, password)
+            if len(decoded_password) != 0:
+                decoded_passwords.append(decoded_password)
+
+        return decoded_passwords
 
 
 class SchemaParser:
@@ -68,7 +105,7 @@ class SchemaParser:
 
             self.fields.append(name)
 
-        print(self.fields)
+        #print(self.fields)
         self.Row = namedtuple('Row', self.fields)
 
     def feed_row(self, row):
@@ -92,12 +129,11 @@ class SchemaParser:
             return
 
         if 'gov.uk' in row.origin_url:
-            decoded_password = self.decoders[password_hash_version].decode(password_data)
+            decoded_passwords = self.decoders[password_hash_version].decode(password_data)
 
-            print(f'{row.origin_url}: {row.username_value}: '
+            print(f'url: {row.origin_url}, username: {row.username_value}, '
                   f'version: {password_hash_version}, '
-                  f'password_len: {len(password_data)}, '
-                  f'password: {password_data} -> {decoded_password}')
+                  f'password: {binascii.hexlify(password_data)} -> {decoded_passwords}')
 
 def main():
     parser = argparse.ArgumentParser()
